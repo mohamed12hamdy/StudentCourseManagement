48ef4c71177353fb28412624c1c703da7a03063f
MohamedHamdy <midohamdi487@gmail.com>
2025-05-20 01:18:45 +0300

    Add jwt and security module

---
+++ StudentManagement/src/main/java/com/example/StudentManagement/Admin/AdminService.java
@@ -0,0 +1,22 @@
+package com.example.StudentManagement.Admin;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+@Service
+public class AdminService implements UserDetailsService {
+    @Autowired
+    private AdminRepository adminRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Admin admin = adminRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Admin not found"));
+
+        return new User(admin.getUserName(), admin.getPassword(), new ArrayList<>());
+    }
+}
---
+++ StudentManagement/src/main/java/com/example/StudentManagement/Admin/AuthController.java
@@ -0,0 +1,11 @@
+package com.example.StudentManagement.Admin;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+
+}
